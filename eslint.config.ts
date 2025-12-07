070ef6a2c763452db7c1ab53c81b82c044d08a93
Nilok Bose <cosmocoder@gmail.com>
2025-12-07 11:42:05 +0100

    chore: configure ESLint

---
+++ eslint.config.ts
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['node_modules', 'build']),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: {
+      js
+    },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  tseslint.configs.recommended,
+]);
